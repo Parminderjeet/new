c5ad316a766d0dac2ed8f014af687ffb36e8a70c
Parminderjeet <parminderjeetkaur73@gmail.com>
2021-06-02 16:28:03 -0700

    “newone”

---
+++ simple.py
@@ -0,0 +1,13 @@
+import streamlit as st
+# Title
+st.title("Streamlit demo ")
+# Header
+st.header("Icfoss")
+# Subheader
+st.subheader("Language&Technology")
+
+# Text
+st.text("ML BATCH")
+
+# Markdown
+st.markdown("## Streamlit  ")
